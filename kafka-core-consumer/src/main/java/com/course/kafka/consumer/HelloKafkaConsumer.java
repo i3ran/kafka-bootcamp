92f8bdd597c0dce9523e1e82b766866a2112afe1
@UnityLearner65 <ziyad.i.ansari@gmail.com>
2024-03-29 08:21:59 +1100

    Initial producer and consumer with docker infra

---
+++ kafka-core-consumer/src/main/java/com/course/kafka/consumer/HelloKafkaConsumer.java
@@ -0,0 +1,13 @@
+package com.course.kafka.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloKafkaConsumer {
+
+    @KafkaListener(topics = "t-hello")
+    public void consume(String msg){
+        System.out.println("----------------Message received: " + msg);
+    }
+}
